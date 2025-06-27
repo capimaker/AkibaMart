dede6acd7bce11bb8174d18aa78266aa7139630e
capimaker <capimaker@gmail.com>
2025-06-27 19:39:35 +0200

    se ha modificado products optimizando el codigo

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ function App() {
                   } />
                   <Route path="/register" element={<Register />}/>
               <Route path="/login" element={<Login />}/>
-              
               <Route path="/products" element={<Products />} />
                {/*} <Route path="/cart" element={<Cart />} />*/}
             </Routes>
