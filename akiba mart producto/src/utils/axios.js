87ae21542ecccac4eebca2a797677010a2285ca4
capimaker <capimaker@gmail.com>
2025-06-28 17:07:38 +0200

    se ha eliminado una carpeta repetida

--- akiba mart producto/src/utils/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-const axios = axios.create({
-  baseURL: "https://akibapi.onrender.com",
-  headers: {
-    "Content-Type": "application/json"
-  }
-});
-export default axios;
